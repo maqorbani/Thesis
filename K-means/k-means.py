296221730c23ff311a71a32b8578b09a21e7928b
MohammadAli Qorbani <qorbani.maq@gmail.com>
2020-12-10 11:56:09 +0330

    Cluster centroids distance matrix

--- K-means/k-means.py
+++ K-means/k-means.py
@@ -8,8 +8,8 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 # get_ipython().run_line_magic('matplotlib', 'inline')
 
-'''
 # %%
+'''
 with open('Altitude.txt') as f:
     alt = f.read()
     alt = alt.split('\n')
@@ -32,15 +32,16 @@ with open('difHorRad.txt') as f:
     dif = f.read()
     dif = dif.split('\n')
     dif = [float(i) for i in dif]
-
 # %%
-print(len(alt), len(azi), len(dire), len(dif))
+
 '''
 # %%
 alt = np.loadtxt('Altitude.txt')
-azi = np.loadtxt('Azimuth.txt')
+azi = np.loadtxt('Azimuth.txt') - 180
 dire = np.loadtxt('dirRad.txt')
 dif = np.loadtxt('difHorRad.txt')
+key = np.loadtxt('key.txt', dtype='str')
+print(len(alt), len(azi), len(dire), len(dif))
 
 # %%
 plt.scatter(azi, alt, s=10)
@@ -92,3 +93,33 @@ distances = euclidean_distance(sunData, kmeans.cluster_centers_)
 selected = []
 for i in distances.values():
     selected.append([alt[i[1]], azi[i[1]], dire[i[1]], dif[i[1]]])
+
+selected = np.array(selected)
+
+# %%
+plt.scatter(azi, alt, s=2, c='grey')
+plt.scatter(selected[:, 1], selected[:, 0], s=10, c='red')
+plt.xlabel('azimuth')
+plt.ylabel('altitude')
+plt.show()
+
+# %%
+plt.scatter(dire, dif, s=2, c='grey')
+plt.scatter(selected[:, 2], selected[:, 3], s=10, c='red')
+plt.xlabel('direct rad')
+plt.ylabel('diffuse rad')
+plt.show()
+
+# %%
+for i in distances:
+    print(key[distances[i][1]])
+
+# FIND OUT IF THERE ARE ANY coincidental CENTROIDS
+# %%
+coincidental = np.zeros((k, k))
+center = kmeans.cluster_centers_
+
+for i in range(k):
+    coincidental[:, i] = np.linalg.norm((center-center[i]), axis=1)
+
+(coincidental+np.eye(200)*coincidental.max()).min(axis=0)  # Gets dist matrix
