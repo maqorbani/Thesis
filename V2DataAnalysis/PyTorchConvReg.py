2a4f9a505d341f3de8e1401e9868ffa5b35f347a
MohammadAli Qorbani <qorbani.maq@gmail.com>
2021-01-06 18:23:10 +0330

    new 600 neuron branch for AOmap

--- V2DataAnalysis/PyTorchConvReg.py
+++ V2DataAnalysis/PyTorchConvReg.py
@@ -48,7 +48,8 @@ x_test = np.transpose(x_test, [0, 2, 1]).reshape(-1, n_features, 144, 256)
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.AAconv = nn.Conv2d(2, 400, 1)
+        self.AAconv = nn.Conv2d(1, 400, 1)
+        self.AOconv = nn.Conv2d(1, 600, 1)
 
         self.BBconv1 = nn.Conv2d(n_features - 2, 600, 1)
         self.BBconv2 = nn.Conv2d(600, 600, 1)
@@ -57,7 +58,7 @@ class Model(nn.Module):
         self.BBconv5 = nn.Conv2d(600, 600, 1)
         self.BBconv6 = nn.Conv2d(600, 600, 1)
 
-        self.CCconv = nn.Conv2d(1000, 600, 1)
+        self.CCconv = nn.Conv2d(1600, 600, 1)
         self.out = nn.Conv2d(600, 1, 1)
 
         self.m = nn.Dropout(p=0.2)
@@ -66,13 +67,17 @@ class Model(nn.Module):
         x = x.to(device)
 
         # (1, 144, 256) sunpatch image, reshaping for avoiding a size 1 array
-        xA = x[-2:, :, :]
-        xA = xA.reshape(1, 2, xA.shape[-2], xA.shape[-1])  # (1, 1, 144, 256)
+        xA = x[-1, :, :]
+        xA = xA.reshape(1, 1, xA.shape[-2], xA.shape[-1])  # (1, 1, 144, 256)
+        xO = x[-2, :, :]
+        xO = xO.reshape(1, 1, xO.shape[-2], xO.shape[-1])  # (1, 1, 144, 256)
         # (7, 144, 256) other than sunpatch
-        xB = x[:-2, :, :].unsqueeze(0)                     # (1, 7, 144, 256)
+        xB = x[:-2, :, :].unsqueeze(0)                     # (1, n, 144, 256)
         del x
 
         xA = F.relu(self.AAconv(xA))
+
+        xO = F.relu(self.AOconv(xO))
 
         xB = F.relu(self.BBconv1(xB))
         xB = F.relu(self.BBconv2(xB))
@@ -81,8 +86,9 @@ class Model(nn.Module):
         xB = F.relu(self.BBconv5(xB))
         xB = F.relu(self.BBconv6(xB))
 
-        xB = torch.cat([xA, xB], dim=1)
+        xB = torch.cat([xA, xO, xB], dim=1)
         del xA
+        del xO
         xB = F.relu(self.CCconv(xB))
         xB = F.relu(self.out(xB))
 
@@ -114,7 +120,7 @@ epochLossBatch = []
 testLossBatch = []
 
 # %%
-optimizer = optim.Adam(model.parameters(), 0.0001)
+optimizer = optim.Adam(model.parameters(), 0.000005)
 # model.zero_grad()   # zero the gradient buffe/rs
 
 
