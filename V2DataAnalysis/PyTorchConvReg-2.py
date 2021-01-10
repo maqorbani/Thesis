9520a739d4f98c75291f41d5e8fd964457203086
MohammadAli Qorbani <qorbani.maq@gmail.com>
2021-01-10 12:43:58 +0330

    new convReg arch was added from AOmap

---
+++ V2DataAnalysis/PyTorchConvReg-2.py
@@ -0,0 +1,263 @@
+# %%
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+from GPUtil import showUtilization as gpu_usage
+
+import pytorch_ssim
+
+# %%
+Dictionary = {
+    'epoch': 5,
+    'batch': 8,
+    'dataset': '-NM-AO',
+    'View #': 2
+}
+
+# %%
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+epoch = Dictionary['epoch']
+batch = Dictionary['batch']
+
+data_set = Dictionary['dataset']                             # Data-sets
+View = Dictionary['View #']
+
+x_train = np.load(f'../V{View}DataAnalysis/data/data' +
+                  data_set + '.npz')['train']
+x_test = np.load(f'../V{View}DataAnalysis/data/data' +
+                 data_set + '.npz')['test']
+
+n_features = x_train.shape[-1] - 1
+m = x_train.shape[0]
+mTest = x_test.shape[0]
+# %%
+# Transforming the data into torch tensors.
+x_train, y_train = torch.tensor(x_train[:, :, :n_features]), \
+    torch.tensor(x_train[:, :, -1])
+x_test, y_test = torch.tensor(x_test[:, :, :n_features]), \
+    torch.tensor(x_test[:, :, -1])
+
+# relocation of the channel axes
+x_train = np.transpose(x_train, [0, 2, 1]).reshape(-1, n_features, 144, 256)
+x_test = np.transpose(x_test, [0, 2, 1]).reshape(-1, n_features, 144, 256)
+
+# %%
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.AAconv = nn.Conv2d(1, 400, 1)
+        self.AOconv = nn.Conv2d(1, 600, 1)
+
+        self.BBconv1 = nn.Conv2d(n_features - 2, 600, 1)
+        self.BBconv2 = nn.Conv2d(1200, 600, 1)
+        self.BBconv3 = nn.Conv2d(600, 600, 1)
+        self.BBconv4 = nn.Conv2d(600, 600, 1)
+        self.BBconv5 = nn.Conv2d(600, 600, 1)
+        self.BBconv6 = nn.Conv2d(600, 600, 1)
+
+        self.CCconv = nn.Conv2d(1000, 600, 1)
+        self.out = nn.Conv2d(600, 1, 1)
+
+        self.m = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        x = x.to(device)
+
+        # (1, 144, 256) sunpatch image, reshaping for avoiding a size 1 array
+        xA = x[-1, :, :]
+        xA = xA.reshape(1, 1, xA.shape[-2], xA.shape[-1])  # (1, 1, 144, 256)
+        xO = x[-2, :, :]
+        xO = xO.reshape(1, 1, xO.shape[-2], xO.shape[-1])  # (1, 1, 144, 256)
+        # (7, 144, 256) other than sunpatch
+        xB = x[:-2, :, :].unsqueeze(0)                     # (1, n, 144, 256)
+        del x
+
+        xA = F.relu(self.AAconv(xA))
+
+        xO = F.relu(self.AOconv(xO))
+
+        xB = F.relu(self.BBconv1(xB))
+        xB = torch.cat([xB, xO], dim=1)
+        del xO
+        xB = F.relu(self.BBconv2(xB))
+        xB = F.relu(self.BBconv3(xB))
+        xB = F.relu(self.BBconv4(xB))
+        xB = F.relu(self.BBconv5(xB))
+        xB = F.relu(self.BBconv6(xB))
+
+        xB = torch.cat([xA, xB], dim=1)
+        del xA
+        xB = F.relu(self.CCconv(xB))
+        xB = F.relu(self.out(xB))
+
+        return xB
+
+
+# %%
+model = Model()
+model.to(device)
+
+print(model)
+gpu_usage()
+
+# %%
+with torch.no_grad():
+    out = model(x_train[3, :, :, :])
+    plt.imshow(out.to("cpu").numpy().reshape(144, -1))
+    plt.show()
+
+torch.cuda.empty_cache()
+gpu_usage()
+
+# %%
+epochLoss = []
+criterion = nn.MSELoss()
+testLoss = []
+
+epochLossBatch = []
+testLossBatch = []
+
+# %%
+optimizer = optim.Adam(model.parameters(), 0.000008)
+# model.zero_grad()   # zero the gradient buffe/rs
+
+
+# %%
+
+epochPercent = 0  # Dummy variable, just for printing purposes
+model.train()
+
+for i in range(epoch*m):
+    target = y_train[i % m, :].reshape(-1, 1)  # avoiding 1D array
+    x = x_train[i % m, :, :, :]
+    output = model(x).cpu().reshape(-1, 1)
+    loss = criterion(output, target)
+    loss.backward()
+
+    epochLoss.append(loss.item())
+    epochLossBatch.append(epochLoss[-1])
+
+    with torch.no_grad():  # Calculating the test-set loss
+        test_target = y_test[i % mTest, :].reshape(-1, 1)
+        xTe = x_test[i % mTest, :, :, :]
+        output = model(xTe).cpu().reshape(-1, 1)
+        testLoss.append(criterion(output, test_target).item())
+        testLossBatch.append(testLoss[-1])
+
+    if i % batch == batch - 1:
+        optimizer.step()
+        model.zero_grad()
+
+    if (i + 1) * 10 // m == epochPercent + 1:
+        print("#", end='')
+        epochPercent += 1
+
+    if i % m == m - 1:
+        print('\n', "-->>Train>>", sum(epochLossBatch)/m)
+        print("-->>Test>>", sum(testLossBatch)/m)
+        epochLossBatch = []
+        testLossBatch = []
+
+# %%
+plt.plot(np.log10(epochLoss))
+plt.plot(np.log10(testLoss))
+
+a = np.array(epochLoss)
+for i in range(int(len(epochLoss) / m)):
+    a[i*m:((i+1)*m)] = a[i*m:((i+1)*m)].mean()
+plt.plot(np.log10(a), lw=4)
+
+plt.show()
+# %%
+model.eval()
+number = 152
+with torch.no_grad():
+    out = model(x_train[number, :, :]).to(
+        "cpu").numpy().reshape(144, -1)+0.01
+    T = y_train[number, :].to("cpu").numpy().reshape(144, -1)+0.01
+# plt.imshow((out.to("cpu").detach().numpy().reshape(144, -1)))
+# plt.show()
+
+# Plotting both prediction and target images
+fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(40, 10))
+ax1.imshow(np.log10(out))
+ax1.title.set_text(f'{number}\nprediction')
+ax2.imshow(np.log10(T))
+ax2.title.set_text('ground_truth')
+ax3.imshow(np.abs(out-T), vmax=0.2)
+ax3.title.set_text('difference')
+# plt.imshow((y_train[5, :].to("cpu").detach().numpy().reshape(144, -1))
+#            - (out.to("cpu").detach().numpy().reshape(144, -1)), vmax=0.67)
+plt.show()
+
+# %%
+fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(7, 10))
+ax1.hist((out).ravel(), bins=50)
+ax1.title.set_text('y_hat')
+ax2.hist((T).ravel(), bins=50)
+ax2.title.set_text('ground_truth')
+ax3.hist(np.abs(out-T).ravel(), bins=50)
+ax3.title.set_text('difference')
+
+plt.show()
+# %%
+torch.save(model.state_dict(),
+           f'../V{View}DataAnalysis/ConvModel{data_set}-2.pth')
+
+# %%
+model.load_state_dict(torch.load(
+    f'../V{View}DataAnalysis/ConvModel{data_set}.pth'))
+
+# %%
+# For transfer learning model load
+learnedView = 2
+model.load_state_dict(torch.load(
+    f'../V{learnedView}DataAnalysis/ConvModel{data_set}.pth'))
+
+# %%
+# Loss calculator over the train-test sets
+train_loss = []
+test_loss = []
+
+with torch.no_grad():
+    for i in range(m):
+        target = y_train[i, :].reshape(-1, 1)  # avoiding 1D array
+        x = x_train[i, :, :, :]
+        output = model(x).cpu().reshape(-1, 1)
+        loss = criterion(output, target)
+        train_loss.append(loss.item())
+    for i in range(mTest):
+        target = y_test[i, :].reshape(-1, 1)  # avoiding 1D array
+        x = x_test[i, :, :, :]
+        output = model(x).cpu().reshape(-1, 1)
+        loss = criterion(output, target)
+        test_loss.append(loss.item())
+
+print(sum(train_loss)/m)
+print(sum(test_loss)/mTest)
+
+# %%
+train_ssim = []
+test_ssim = []
+
+with torch.no_grad():
+    for i in range(m):
+        target = y_train[i, :].reshape(1, 1, 144, 256)
+        x = x_train[i, :, :, :]
+        output = model(x).cpu()
+        loss = pytorch_ssim.ssim(target, output)
+        train_ssim.append(loss.item())
+    for i in range(mTest):
+        target = y_test[i, :].reshape(1, 1, 144, 256)
+        x = x_test[i, :, :, :]
+        output = model(x).cpu()
+        loss = pytorch_ssim.ssim(target, output)
+        test_ssim.append(loss.item())
+
+print(sum(train_ssim)/m)
+print(sum(test_ssim)/mTest)
