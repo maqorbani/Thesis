0f7950a874ce5ce3f2d3211b1cda00f544d53358
MohammadAli Qorbani <qorbani.maq@gmail.com>
2021-01-29 14:21:27 +0330

    model 3 and 4, The conv kernel>1 models were added

--- V2DataAnalysis/PyTorchModel.py
+++ V2DataAnalysis/PyTorchModel.py
@@ -1,4 +1,3 @@
-# %%
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -101,18 +100,23 @@ class Model_2(nn.Module):
 
 
 class Model_3(nn.Module):
-    def __init__(self, n_features, device):
+    def __init__(self, n_features, device, pixel):
         super(Model_3, self).__init__()
 
         self.n_features = n_features
         self.device = device
 
+        self.kernel = pixel*2 + 1
+        self.padding = pixel
+
         self.AAconv = nn.Conv2d(1, 400, 1)
         self.AOconv = nn.Conv2d(1, 600, 1)
 
-        self.BBconv1 = nn.Conv2d(n_features - 2, 600, 1)
-        self.BBconv2 = nn.Conv2d(600, 600, 1)
-        self.BBconv3 = nn.Conv2d(1200, 600, 1)
+        self.BBconv1 = nn.Conv2d(
+            n_features - 2, 600, self.kernel, padding=self.padding,
+            padding_mode='reflect')
+        self.BBconv2 = nn.Conv2d(1200, 600, 1)
+        self.BBconv3 = nn.Conv2d(600, 600, 1)
         self.BBconv4 = nn.Conv2d(600, 600, 1)
         self.BBconv5 = nn.Conv2d(600, 600, 1)
         self.BBconv6 = nn.Conv2d(600, 600, 1)
@@ -139,14 +143,67 @@ class Model_3(nn.Module):
         xO = F.relu(self.AOconv(xO))
 
         xB = F.relu(self.BBconv1(xB))
-        xB = F.relu(self.BBconv2(xB))
         xB = torch.cat([xB, xO], dim=1)
         del xO
+        xB = F.relu(self.BBconv2(xB))
         xB = F.relu(self.BBconv3(xB))
         xB = F.relu(self.BBconv4(xB))
         xB = F.relu(self.BBconv5(xB))
         xB = F.relu(self.BBconv6(xB))
 
+        xB = torch.cat([xA, xB], dim=1)
+        del xA
+        xB = F.relu(self.CCconv(xB))
+        xB = F.relu(self.out(xB))
+
+        return xB
+
+
+class Model_4(nn.Module):
+    def __init__(self, n_features, device, pixel):
+        super(Model_4, self).__init__()
+
+        self.n_features = n_features
+        self.device = device
+
+        self.kernel = pixel*2 + 1
+        self.padding = pixel
+
+        self.AAconv = nn.Conv2d(1, 400, 1)
+
+        self.BBconv1 = nn.Conv2d(
+            n_features - 1, 600, self.kernel, padding=self.padding,
+            padding_mode='reflect')
+        self.BBconv2 = nn.Conv2d(600, 600, 1)
+        self.BBconv3 = nn.Conv2d(600, 600, 1)
+        self.BBconv4 = nn.Conv2d(600, 600, 1)
+        self.BBconv5 = nn.Conv2d(600, 600, 1)
+        self.BBconv6 = nn.Conv2d(600, 600, 1)
+
+        self.CCconv = nn.Conv2d(1000, 600, 1)
+        self.out = nn.Conv2d(600, 1, 1)
+
+        # self.m = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        x = x.to(self.device)
+
+        # (1, 144, 256) sunpatch image, reshaping for avoiding a size 1 array
+        xA = x[-1, :, :]
+        xA = xA.reshape(1, 1, xA.shape[-2], xA.shape[-1])  # (1, 1, 144, 256)
+
+        # (7, 144, 256) other than sunpatch
+        xB = x[:-1, :, :].unsqueeze(0)                     # (1, n, 144, 256)
+        del x
+
+        xA = F.relu(self.AAconv(xA))
+
+        xB = F.relu(self.BBconv1(xB))
+        xB = F.relu(self.BBconv2(xB))
+        xB = F.relu(self.BBconv3(xB))
+        xB = F.relu(self.BBconv4(xB))
+        xB = F.relu(self.BBconv5(xB))
+        xB = F.relu(self.BBconv6(xB))
         xB = torch.cat([xA, xB], dim=1)
         del xA
         xB = F.relu(self.CCconv(xB))
