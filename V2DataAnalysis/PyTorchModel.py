38e7860764dc7b729d748552632f89dc9c444245
MohammadAli Qorbani <qorbani.maq@gmail.com>
2021-02-10 14:16:00 +0330

    pyTrochModel 5 & 6 were added (inception net)

--- V2DataAnalysis/PyTorchModel.py
+++ V2DataAnalysis/PyTorchModel.py
@@ -210,3 +210,113 @@ class Model_4(nn.Module):
         xB = F.relu(self.out(xB))
 
         return xB
+
+
+class Model_5(nn.Module):
+    '''
+    Passing the normal map through a convNet 3*(default) before
+    concatenating it to the main channels
+    '''
+
+    def __init__(self, n_features, device, NMkernel=3):
+        super(Model_5, self).__init__()
+        self.n_features = n_features
+        self.device = device
+        self.NMkernel = NMkernel
+
+        self.AAconv = nn.Conv2d(1, 400, 1)
+
+        self.NMconv = nn.Conv2d(3, 1, self.NMkernel, padding=1)
+
+        self.BBconv1 = nn.Conv2d(n_features - 3, 600, 1)
+        self.BBconv2 = nn.Conv2d(600, 600, 1)
+        self.BBconv3 = nn.Conv2d(600, 600, 1)
+        self.BBconv4 = nn.Conv2d(600, 600, 1)
+
+        self.CCconv = nn.Conv2d(1000, 600, 1)
+        self.out = nn.Conv2d(600, 1, 1)
+
+    def forward(self, x):
+        x = x.to(self.device)
+
+        NM = x[-4:-1, :, :].unsqueeze(0)
+        NM = F.relu(self.NMconv(NM))
+
+        # (36864, 1) the sunpatch images, reshaping for avoiding a size 1 array
+        xA = x[-1, :, :]
+        xA = xA.reshape(1, 1, xA.shape[-2], xA.shape[-1])
+        # (36864, 7) other than sunpatch
+        xB = x[:-4, :, :].unsqueeze(0)
+        xB = torch.cat([xB, NM], dim=1)
+        del x
+
+        xA = F.relu(self.AAconv(xA))
+
+        xB = F.relu(self.BBconv1(xB))
+        xB = F.relu(self.BBconv2(xB))
+        xB = F.relu(self.BBconv3(xB))
+        xB = F.relu(self.BBconv4(xB))
+
+        xB = torch.cat([xA, xB], dim=1)
+        del xA
+        xB = F.relu(self.CCconv(xB))
+        xB = F.relu(self.out(xB))
+
+        return xB
+
+
+class Model_6(nn.Module):
+    '''
+    Passing the normal map through an inception network
+    before concatenating it to the main channels
+    '''
+
+    def __init__(self, n_features, device):
+        super(Model_6, self).__init__()
+        self.n_features = n_features
+        self.device = device
+
+        self.AAconv = nn.Conv2d(1, 400, 1)
+
+        self.NMconv1 = nn.Conv2d(3, 3, 1, padding=0)
+        self.NMconv3 = nn.Conv2d(3, 3, 3, padding=1)
+        self.NMconv5 = nn.Conv2d(3, 3, 5, padding=2)
+
+        self.BBconv1 = nn.Conv2d(n_features + 5, 600, 1)
+        self.BBconv2 = nn.Conv2d(600, 600, 1)
+        self.BBconv3 = nn.Conv2d(600, 600, 1)
+        self.BBconv4 = nn.Conv2d(600, 600, 1)
+
+        self.CCconv = nn.Conv2d(1000, 600, 1)
+        self.out = nn.Conv2d(600, 1, 1)
+
+    def forward(self, x):
+        x = x.to(self.device)
+
+        NM = x[-4:-1, :, :].unsqueeze(0)
+        NM1 = F.relu(self.NMconv1(NM))
+        NM3 = F.relu(self.NMconv3(NM))
+        NM5 = F.relu(self.NMconv5(NM))
+        del NM
+
+        # (36864, 1) the sunpatch images, reshaping for avoiding a size 1 array
+        xA = x[-1, :, :]
+        xA = xA.reshape(1, 1, xA.shape[-2], xA.shape[-1])
+        # (36864, 7) other than sunpatch
+        xB = x[:-4, :, :].unsqueeze(0)
+        xB = torch.cat([xB, NM1, NM3, NM5], dim=1)
+        del x, NM1, NM3, NM5
+
+        xA = F.relu(self.AAconv(xA))
+
+        xB = F.relu(self.BBconv1(xB))
+        xB = F.relu(self.BBconv2(xB))
+        xB = F.relu(self.BBconv3(xB))
+        xB = F.relu(self.BBconv4(xB))
+
+        xB = torch.cat([xA, xB], dim=1)
+        del xA
+        xB = F.relu(self.CCconv(xB))
+        xB = F.relu(self.out(xB))
+
+        return xB
