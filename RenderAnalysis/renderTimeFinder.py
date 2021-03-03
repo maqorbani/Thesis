a4d7ccb8dfb45398ea0510fe994717483d85011e
MohammadAli Qorbani <qorbani.maq@gmail.com>
2021-03-03 19:19:40 +0330

    updated render time finder

--- RenderAnalysis/renderTimeFinder.py
+++ RenderAnalysis/renderTimeFinder.py
@@ -1,10 +1,19 @@
+# %%
 import re
 import os
+import numpy as np
 
 pattern = re.compile(r'(\d.\d{3})r')
 
-for i in os.listdir('TheRender/Octs/'):
-    with open(f'TheRender/Octs/{i}/error.log') as f:
-        rTime = f.readlines()[-1]
-    
+times = []
+
+for i in os.listdir():
+    if os.path.isdir(i):
+        with open(f'{i}/error.log') as f:
+            rTime = f.readlines()[-1]
+
     print(float(re.findall(pattern, rTime)[0]))
+
+    times.append(float(re.findall(pattern, rTime)[0]))
+
+times = np.array(times)
