fa5a2c08625b4568d445f0d9600d9311b6ca35c2
MohammadAli Qorbani <qorbani.maq@gmail.com>
2021-08-07 18:49:33 +0430

    main.py was updated

--- RenderFilesArchive/Panorama/main.py
+++ RenderFilesArchive/Panorama/main.py
@@ -1,34 +1,36 @@
+# %%
 import os
 import subprocess
 import shutil
 import concurrent.futures
 import numpy as np
 import io
+import time
 
+# %%
 # Variables
-ab = 4
+ab = 4  # Either 4 or 0
 nCPU = 64
-All_skies = False
+nCores = 1  # Number of rtrace threads
+imgDims = 256  # output would be: imgDims * imgDims/2
 # End of Variables
-
-# For min-max finder
-minMaxDict = {i: [100, 0] for i in range(nCPU)}
 
 # os.chdir('Desktop/TheRender/')
 print('Current working dir is: ' + os.getcwd())
 
-if not All_skies and os.path.exists('render_list.txt'):
-    skies = np.loadtxt('render_list.txt', dtype=str)
-    skies = [f'{i}__.sky' for i in skies]
-else:
-    skies = os.listdir('skies/')  # 4399
+# Set the skies
+skies = [i[:-4] for i in os.listdir('../skies/')]
 # skies = skies[:3]  # To test a few number
+
+# For min-max finder
+minMaxDict = {i: [100, 0] for i in range(nCPU)}
 
 # Overview
 print(f'Number of HDRs to be rendered: {len(skies)}')
 print(f'Number of CPUs: {nCPU}')
 print(f'Number of Ambient Bounces: {ab}')
 
+# Creating the Octs dir if not existant
 if not os.path.exists('Octs'):
     os.mkdir('Octs')
 
@@ -39,14 +41,14 @@ for i in os.listdir('Octs/'):
     if os.path.isdir('Octs/'+i):
         if not os.path.exists(f'Octs/{i}/done'):
             dirs.append(i)
-            shutil.rmtree('Octs/'+i)
+            shutil.rmtree(f'Octs/{i}')
         else:
             try:
-                skies.remove(f'{i}__.sky')
+                skies.remove(f'{i}.sky')
             except ValueError:
-                print('Warning! x not in list')
+                print(f'Warning! {i} not in list')
 
-# print(skies) 
+# Dividing skies into CPU clusters
 divisionCPU = len(skies) // nCPU
 
 skyDict = {}
@@ -57,7 +59,6 @@ for i, sky in enumerate(skies[nCPU * divisionCPU:]):
     skyDict['sky'+str(i)].append(sky)
 print(skyDict)
 # print(len(list(skyDict.values())[5]))
-
 
 # Changing the directory
 os.chdir('Octs')
@@ -70,61 +71,59 @@ os.environ['RAYPATH'] = '.:/usr/local/radiance/lib'
 badDirs = []
 
 
+# %%
 def octMaker(sky, j):
     for i in sky:
+        # Timing
+        a = time.time()
+
         # make the oconv command
-        oconv = f'oconv ../skies/{i} ../Geo.rad > {i[:-6]}/tower.oct'
+        oconv = f'oconv ../../skies/{i}.sky ../Geo.rad > {i}/render.oct'
 
         # make the rpict command
-        render = f'rpict -t 60 -vth -vh 180 -vv 180 -x 800 -y 800 \
--ps 2 -pt 0.05 -pj 0.9 -dj 0.7 -ds 0.15 -dt 0.05 -dc 0.75 -dr 3 -dp 512 \
--st 0.15 -ab {ab} -ad 2048 -as 1024 -ar 512 -aa 0.1 -lr 8 -lw 0.005 \
--e {i[:-6]}/error.log {i[:-6]}/tower.oct > {i[:-6]}/{i[:-6]}.HDR'
-
-        # Make the pfilt command
-        pfilt = f'pfilt -x /4 -y /4 {i[:-6]}/{i[:-6]}.HDR > {i[:-6]}/{i[:-6]}_1.HDR'
+        render = f'X={imgDims}; Y={imgDims//2}; cnt $Y $X | rcalc -f ../2d360.cal ' + \
+                 '-e "XD=$X;YD=$Y;X=5.0;Y=3.5;Z=1.2" | rtrace -n {nCores}' + \
+                 '-x $X -y $Y -fac @../savedAB{ab}.opt {i}/render.oct > {i}/{i}.hdr'
 
         # Make the render matrix
-        pvalue = f'pvalue -h -d -H -b -o {i[:-6]}/{i[:-6]}_1.HDR > {i[:-6]}/{i[:-6]}.txt'
+        pvalue = f'pvalue -h -d -H -b -o {i}/{i}.hdr > {i}/{i}.txt'
 
         # Make the render directory
-        os.mkdir(str(i[:-6]))
+        os.mkdir(f'{i}')
 
         # Make the octree file
         os.system(oconv)
 
         # renders using rpict & remove the octree afterwards
         os.system(f'{render}')
-        os.remove(f'{i[:-6]}/tower.oct')
-
-        # Downsampling rendered images
-        os.system(pfilt)
+        os.remove(f'{i}/render.oct')
 
         if os.system(pvalue):
-            print(i[:-6], ' is bad image.')
-            badDirs.append(i[:-6])
-            os.remove(f'{i[:-6]}/{i[:-6]}.txt')
+            print(i, ' is bad image.')
+            badDirs.append(i)
+            os.remove(f'{i}/{i}.txt')
             continue
         else:
-            print(i[:-6])
-        with io.open(f'{i[:-6]}/{i[:-6]}.txt', 'r', encoding=None) as f:
+            print(i)
+        with io.open(f'{i}/{i}.txt', 'r', encoding=None) as f:
             vector = np.loadtxt(f, delimiter='\n')
             MIN, MAX = vector.min(), vector.max()
             if MIN < minMaxDict[j][0]:
                 minMaxDict[j][0] = MIN
             if MAX > minMaxDict[j][1]:
                 minMaxDict[j][1] = MAX
-            np.savetxt(f'{i[:-6]}/{i[:-6]}.gz', vector)
-        os.remove(f'{i[:-6]}/{i[:-6]}.txt')
+            np.savetxt(f'{i}/{i}.gz', vector)
+        os.remove(f'{i}/{i}.txt')
 
         # Finished statement
-        os.system(f'touch {i[:-6]}/done')
-        print(str(i[:-6] + ' done!'))
+        os.system(f'touch {i}/done')
+        os.system(f'echo "{time.time() - a:.2f}" > done')
+        print(str(i + ' done!'))
 
     return 'Process #' + str(j) + ' is done!'
 
 
-
+# %%
 # The parallel processing module
 with concurrent.futures.ProcessPoolExecutor() as executor:
     results = [executor.submit(octMaker, list(
@@ -133,6 +132,7 @@ with concurrent.futures.ProcessPoolExecutor() as executor:
     for f in concurrent.futures.as_completed(results):
         print(f.result())
 
+# %%
 # minMax to numpy here
 minMax = np.array(list(minMaxDict.values()))
 MIN = minMax[:, 0].min()
